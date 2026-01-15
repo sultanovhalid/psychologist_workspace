b018a47db3aed5d2f8d638b4735b18ce4d89ff24
sultanovkhalid <Ourblack92@gmail.com>
2026-01-16 02:30:41 +0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    role = db.Column(db.String(16), nullable=False, default='psychologist')  # 'psychologist' or 'admin'
+    role = db.Column(db.String(16), nullable=False, default='psychologist')  # 'psychologist', 'admin', or 'viewer'
     full_name = db.Column(db.String(128), nullable=True)
 
     # Связи с новыми сущностями
